48d72a3da8ff90b3d62d13243074db4a4d4ac3cb
nocilascg <nicolas99cg@gmail.com>
2024-05-17 16:49:01 -0500

    graph finished

--- backend/models/graph.py
+++ backend/models/graph.py
@@ -1,45 +1,116 @@
+import json
+
 class Graph:
     def __init__(self, supply: list[dict] = [], demand:list[dict] = [] , transshipment: list[dict] = []) -> None:
-        self.supply : list[SupplyNode]
-        self.demand : list[DemandNode]
-        self.transshipment : list[Node]
+        self.supply : list[SupplyNode] = []
+        self.demand : list[DemandNode] = []
+        self.transshipment : list[Node] = []
         self.transitions : list[Transition] = []
         self.estructure(supply,demand,transshipment)
         
     def estructure(self,supply:list[dict], demand:list[dict], transshipment:list[dict]):
         for supply_node in supply:
             name = supply_node.get("name")
-            supply_quantity = supply_node.get("capacity")
+            supply_quantity = supply_node.get("supply_quantity")
             self.supply.append(SupplyNode(name,supply_quantity))
             self.add_transitions(supply_node)
         for demand_node in demand:
             name = demand_node.get("name")
-            demand_quantity = demand_node.get("capacity")
+            demand_quantity = demand_node.get("demand_quantity")
             self.demand.append(DemandNode(name,demand_quantity))
             self.add_transitions(demand_node)
         if len(transshipment)>0:
             for transshipment_node in transshipment:
                 name = transshipment_node.get("name")
-                self.demand.append(Node(name))
+                self.transshipment.append(Node(name))
                 self.add_transitions(transshipment_node)
-                
+    
     def add_transitions(self,node_dict: dict):
         origin = node_dict.get("name")
         transitions:list[dict] = node_dict.get("transitions")
         for transition in transitions:
-            cost, destination = next(iter(transition.items()))
+            destination,cost = next(iter(transition.items()))
             self.transitions.append(Transition(origin,destination,cost))
-
-    def to_json(self):
-        if len(self.transshipment)>0:
-            return {'supply': self.supply,
-                    'demand': self.demand,
-                    'transshipment': self.transshipment,
-                    'transitions': self.transitions,}
+        
+    def get_conjunto_i(self):
+        conjunto_i:list[str] = []
+        for supply_node in self.supply:
+            conjunto_i.append(supply_node.name)
+        return conjunto_i
+    
+    def get_conjunto_j(self):
+        conjunto_j:list[str] = []
+        if len(self.transshipment)==0:
+            for demand_node in self.demand:
+                conjunto_j.append(demand_node.name)
         else:
-            return {'supply': self.supply,
-                    'demand': self.demand,
-                    'transitions': self.transitions,}
+            for transshipment_node in self.transshipment:
+                conjunto_j.append(transshipment_node.name)
+        return conjunto_j
+    
+    def get_conjunto_k(self):
+        conjunto_k:list[str] = []
+        for demand_node in self.demand:
+            conjunto_k.append(demand_node.name)
+        return conjunto_k
+                
+    def get_cij(self):
+        CIJ : dict[tuple,float]= {}
+        for transition in self.transitions:
+            if transition.origin in self.get_conjunto_i() and transition.destination in self.get_conjunto_j():
+                CIJ[f'{(transition.origin,transition.destination)}'] = transition.cost
+        return CIJ
+    
+    def get_cjk(self):
+        CJK : dict[tuple,float]= {}
+        for transition in self.transitions:
+            if transition.origin in self.get_conjunto_j() and transition.destination in self.get_conjunto_k():
+                CJK[f'{(transition.origin,transition.destination)}'] = transition.cost
+        return CJK
+    
+    def get_a_oferta(self):
+        a_oferta: dict[str,float] = {}
+        for supply_node in self.supply:
+            a_oferta[supply_node.name] =  supply_node.supply_quantity
+        return a_oferta
+        
+    def get_b_demanda(self):
+        b_demanda: dict[str,float] = {}
+        for demand_node in self.demand:
+            b_demanda[demand_node.name] =  demand_node.demand_quantity
+        return b_demanda
+    
+    def to_json(self):
+        json_dict: dict ={}
+        json_dict["conjunto_i"] = self.get_conjunto_i()
+        json_dict["conjunto_j"] = self.get_conjunto_j()
+        if len(self.transshipment)>0:
+            json_dict["conjunto_k"] = self.get_conjunto_k()
+        json_dict["CIJ"] = self.get_cij()
+        json_dict["CJK"] = self.get_cjk()
+        json_dict["a_oferta"] = self.get_a_oferta()
+        json_dict["b_demanda"] = self.get_b_demanda()
+        return json.dumps(json_dict)
+
+                    
+
+    # def to_json(self):
+    #     return json.dumps(self.obj_to_dict(self), indent=4)
+
+    # def obj_to_dict(self,obj):
+    #     # Si el objeto es una lista o un diccionario, procesar recursivamente
+    #     if isinstance(obj, list):
+    #         return [self.obj_to_dict(i) for i in obj]
+    #     elif isinstance(obj, dict):
+    #         return {k: self.obj_to_dict(v) for k, v in obj.items()}
+        
+    #     # Si el objeto es de una clase definida por el usuario, convertir sus atributos a un diccionario
+    #     elif hasattr(obj, "__dict__"):
+    #         return {k: self.obj_to_dict(v) for k, v in obj.__dict__.items() if not k.startswith('_')}
+        
+    #     # Si el objeto es de un tipo primitivo, devolverlo tal cual
+    #     else:
+    #         return obj
 
 
 class Node:
